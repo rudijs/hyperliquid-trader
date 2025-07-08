3d845aa5fb0ad8c75f82d7d074c297513e54e8f5
Rudi Starcevic <rudi.starcevic@pm.me>
2025-07-08 21:17:10 +0800

    Enable noEmit in TypeScript configuration

--- src/lib/wheel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
-const main = (gear) => {
-    return `Speed is: ${gear.speed}`;
-};
-exports.main = main;
--- src/scratch.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function main(val = 0) {
-    if (val > 9) {
-        return "equal to or greater than 10";
-    }
-    return "less than 10";
-}
-exports.default = main;
